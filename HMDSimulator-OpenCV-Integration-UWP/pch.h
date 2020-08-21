a368b844b105ffaeb9c2c10e57e45008f3bf7ae4
Danilo Gasques <danilod100@gmail.com>
2020-08-20 23:02:37 -0700

    gettings started with HMDSimulator-OpenCV-Integration for UWP

---
+++ HMDSimulator-OpenCV-Integration-UWP/pch.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include "targetver.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+
+#ifndef __OPENCV_CCALIB_PRECOMP__
+#define __OPENCV_CCALIB_PRECOMP__
+
+#include <opencv2/core.hpp>
+#include <opencv2/calib3d.hpp>
+#include <vector>
+
+#endif
